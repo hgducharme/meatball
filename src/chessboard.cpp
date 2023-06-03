778a585c4c1f5ff5fcf08e9986be2ac9a2e5ee55
Hunter Ducharme <hgducharme@gmail.com>
2023-06-02 22:49:44 -0700

    update

--- src/chessboard.cpp
+++ src/chessboard.cpp
@@ -2,19 +2,19 @@
 
 void ChessBoard::init()
 {
-    board[0][0].setBoard(bitboard::DEFAULT_WHITE_PAWN_STRUCTURE);
-    board[0][1].setBoard(bitboard::DEFAULT_WHITE_KNIGHT_STRUCTURE);
-    board[0][2].setBoard(bitboard::DEFAULT_WHITE_BISHOP_STRUCTURE);
-    board[0][3].setBoard(bitboard::DEFAULT_WHITE_ROOK_STRUCTURE);
-    board[0][4].setBoard(bitboard::DEFAULT_WHITE_QUEEN_STRUCTURE);
-    board[0][5].setBoard(bitboard::DEFAULT_WHITE_KING_STRUCTURE);
+    board[WHITE][0].setBoard(bitboard::DEFAULT_WHITE_PAWN_STRUCTURE);
+    board[WHITE][1].setBoard(bitboard::DEFAULT_WHITE_KNIGHT_STRUCTURE);
+    board[WHITE][2].setBoard(bitboard::DEFAULT_WHITE_BISHOP_STRUCTURE);
+    board[WHITE][3].setBoard(bitboard::DEFAULT_WHITE_ROOK_STRUCTURE);
+    board[WHITE][4].setBoard(bitboard::DEFAULT_WHITE_QUEEN_STRUCTURE);
+    board[WHITE][5].setBoard(bitboard::DEFAULT_WHITE_KING_STRUCTURE);
 
-    board[1][0].setBoard(bitboard::DEFAULT_BLACK_PAWN_STRUCTURE);
-    board[1][1].setBoard(bitboard::DEFAULT_BLACK_KNIGHT_STRUCTURE);
-    board[1][2].setBoard(bitboard::DEFAULT_BLACK_BISHOP_STRUCTURE);
-    board[1][3].setBoard(bitboard::DEFAULT_BLACK_ROOK_STRUCTURE);
-    board[1][4].setBoard(bitboard::DEFAULT_BLACK_QUEEN_STRUCTURE);
-    board[1][5].setBoard(bitboard::DEFAULT_BLACK_KING_STRUCTURE);
+    board[BLACK][0].setBoard(bitboard::DEFAULT_BLACK_PAWN_STRUCTURE);
+    board[BLACK][1].setBoard(bitboard::DEFAULT_BLACK_KNIGHT_STRUCTURE);
+    board[BLACK][2].setBoard(bitboard::DEFAULT_BLACK_BISHOP_STRUCTURE);
+    board[BLACK][3].setBoard(bitboard::DEFAULT_BLACK_ROOK_STRUCTURE);
+    board[BLACK][4].setBoard(bitboard::DEFAULT_BLACK_QUEEN_STRUCTURE);
+    board[BLACK][5].setBoard(bitboard::DEFAULT_BLACK_KING_STRUCTURE);
 }
 
 Bitboard ChessBoard::getPieceBitboard(Color color, PieceType piece)
