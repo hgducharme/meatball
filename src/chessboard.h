c837ce705d852660c2f12bbb32808ce4a3964260
Hunter Ducharme <hgducharme@gmail.com>
2023-06-09 15:50:22 -0700

    Changed Chessboard to class instead of stuct

--- src/chessboard.h
+++ src/chessboard.h
@@ -1,7 +1,7 @@
 #include "bitboard.h"
 #include "types.h"
 
-struct Chessboard {
+class Chessboard {
     private:
         Bitboard pieceBitboards_[NUMBER_OF_PIECES];
         Bitboard colorBitboards_[NUMBER_OF_COLORS];
@@ -14,5 +14,4 @@ struct Chessboard {
         const Bitboard & getBitboard(const Color color) const;
         Bitboard getBitboard(const Color color, const PieceType piece) const;
         void movePiece(const Color color, const PieceType piece, const Square startingSquare, const Square endingSquare);
-        void generateMoves();
 };
