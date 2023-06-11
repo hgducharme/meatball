be739d85835b571df069a82eef07506528950bf2
Hunter Ducharme <hgducharme@gmail.com>
2023-06-10 23:34:51 -0700

    Added Rank and File enums

--- src/constants.h
+++ src/constants.h
@@ -9,3 +9,29 @@ namespace constants
       constexpr u64 pawn_single_push = 0x7;
    }
 }
+
+enum File
+{
+   FILE_A,
+   FILE_B,
+   FILE_C,
+   FILE_D,
+   FILE_E,
+   FILE_F,
+   FILE_G,
+   FILE_H,
+   NUMBER_OF_FILES = 8
+};
+
+enum Rank
+{
+   RANK_1,
+   RANK_2,
+   RANK_3,
+   RANK_4,
+   RANK_5,
+   RANK_6,
+   RANK_7,
+   RANK_8,
+   NUMBER_OF_RANKS = 8
+};
