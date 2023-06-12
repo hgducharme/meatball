d8c37fea4764a0adf817a5685b716ec8c34bd0a7
Hunter Ducharme <hgducharme@gmail.com>
2023-06-11 21:37:20 -0700

    saving progress

--- src/attacktables.cpp
+++ src/attacktables.cpp
@@ -26,16 +26,21 @@ constexpr void initializePawnAttackTable()
         // one move to the north west corresponds to the bit 7 positions larger than the current bit
         // one move to the north      corresponds to the bit 8 positions larger than the current bit
         // one move to the north east corresponds to the bit 9 positions larger than the current bit
-        u64 legalMovesFromThisSquare = 0;
+        u64 squareBitboardRepresentation = Bitboard::squareToU64(square);
+        u64 legalMovesFromThisSquare;
         if (Chessboard::squareToFile(square) == FILE_A)
         {
-            u64 disabledNorthWestMoves = constants::attack_masks::pawn_single_push & 0x3;
-            legalMovesFromThisSquare = disabledNorthWestMoves << (square + 8);
-            // Possible ways to improve this:
-            // legalMovesFromThisSquare = square.moveNorth(1)    | square.moveNorthEast(1);
-            // legalMovesFromThisSquare = square.move(North, 1)  | square.move(NorthEast, 1);
-            // legalMovesFromThisSquare = moveNorth(1, square)   | moveNorthEast(1, square);
-            // legalMovesFromThisSquare = getBitboardForMoveNorth(1, currentSquare)
+            // legalMovesFromThisSquare = shift(u64 square, NORTH) | shift(u64 square, NORTH_EAST);
+            // u64 disabledNorthWestMoves = constants::attack_masks::pawn_single_push & 0x3;
+            // legalMovesFromThisSquare = disabledNorthWestMoves << (square + 8);
+            // IDEAL: legalMovesFromThisSquare = shift(u64 square, NORTH) | shift(u64 square, NORTH_EAST)
+            // Input: 64bit integer representation of the square, direction
+            // Output: 64bit integer representation of the square to the <DIRECTION> passed in
+            // name: getSquareFromDirection(u64 square, Direction d)
+            // u64 legalMoves = getSquareFromDirection(squareBitboard, NORTH) | getSquareFromDirection(squareBitboard, NORTH);
+            // legalMovesFromThisSquare.setBoard(legalMoves);
+            // unrelated:
+            // shift(u64 square, direction) { return (square << DIRECTION) } 
         }
         else if (Chessboard::squareToFile(square) == FILE_H)
         {
