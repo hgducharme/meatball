39d6dc6a082679c4569fb2ea07e73ca36a004fb6
Hunter Ducharme <hgducharme@gmail.com>
2023-09-05 16:13:50 -0700

    Removed code

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,7 @@
 #include "bitboard.h"
 #include "chessboard.h"
 
-#include <iostream>
-
 int main ()
 {
-  Chessboard chessboard;
-  std::cout << chessboard.getBitboard(WHITE, PAWN).getBoard() << std::endl;
-
-  // chessboard.setState(FEN);
-  // chessboard.generateMoves();
-
   return 0;
 }
