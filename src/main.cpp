f35129f69ae887006b8fb45a328698458ed7a33f
Hunter Ducharme <hgducharme@gmail.com>
2023-10-04 11:49:04 -0700

    Added comments

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,12 @@
 int main ()
 {
   attack_tables::init();
+
+  // get all the moves = movegenerator.getAllMoves();
+
+  // evalute atll the moves = evaluate;
+
+  // return the best move
   
   return 0;
 }
