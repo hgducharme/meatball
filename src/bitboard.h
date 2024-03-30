53a9d5fb6a2d1798b0f9240bbab3c3cb37533acb
Hunter Ducharme <hgducharme@gmail.com>
2024-03-30 14:02:07 -0700

    Bitboard: added stream insertion overloading and moved all operator overloading to inline non-member functions at bottom of header file

--- src/bitboard.h
+++ src/bitboard.h
@@ -3,6 +3,8 @@
 #include "types.h"
 #include "constants.h"
 
+#include <iostream>
+
 class Bitboard {
     private:
         u64 board_ = constants::EMPTY_BOARD;
@@ -24,41 +26,26 @@ class Bitboard {
         int clearAndReturnMSB();
         Bitboard getNeighbor(Direction direction) const;
 
-        // Shorthand union between this bitboard and another bitboard
-        Bitboard & operator |= (const Bitboard & rhs) { 
-            setBoard(board_ | rhs.getBoard());
-            return *this;
-        }
-
-        // Union between two bitboards
-        friend Bitboard operator | (const Bitboard & a, const Bitboard & b) {
-            return Bitboard(a.getBoard() | b.getBoard());
-        }
-
-        // Shorthand intersection between this bitboard and another bitboard
-        Bitboard & operator &= (const Bitboard & rhs)
-        {
-            setBoard(board_ & rhs.getBoard());
-            return *this;
-        }
-
-        // Intersection between two bitboards
-        friend Bitboard operator & (const Bitboard & a, const Bitboard & b)
-        {
-            return Bitboard(a.getBoard() & b.getBoard());
-        }
-
         private:
             void raiseExceptionIfBitIsOutOfRange(int n) const;
 };
 
+/* Relational operator overloading */
 inline bool operator == (const Bitboard & lhs, const u64 rhs) { return lhs.getBoard() == rhs; }
 inline bool operator == (const Bitboard & lhs, const Bitboard & rhs) { return lhs.getBoard() == rhs.getBoard(); }
 
+/* Bitwise operator overloading */
 inline Bitboard operator << (const Bitboard & lhs, const unsigned int rhs) { return lhs.getBoard() << rhs; }
 inline Bitboard operator >> (const Bitboard & lhs, const unsigned int rhs) { return lhs.getBoard() >> rhs; }
+inline Bitboard operator & (const Bitboard & a, const Bitboard & b) { return Bitboard(a.getBoard() & b.getBoard()); }
+inline Bitboard operator | (const Bitboard & a, const Bitboard & b) { return Bitboard(a.getBoard() | b.getBoard()); }
+inline Bitboard operator ~ (const Bitboard & a) { return Bitboard( ~(a.getBoard()) ); }
+inline Bitboard & operator &= (Bitboard & lhs, const Bitboard & rhs) { lhs.setBoard(lhs.getBoard() & rhs.getBoard()); return lhs; }
+inline Bitboard & operator |= (Bitboard & lhs, const Bitboard & rhs) { lhs.setBoard(lhs.getBoard() | rhs.getBoard()); return lhs; }
 
+/* Arithmetic operator overloading */
 inline u64 operator * (const Bitboard & lhs, const u64 rhs) { return lhs.getBoard() * rhs; }
 inline u64 operator * (const u64 lhs, const Bitboard & rhs) { return lhs * rhs.getBoard(); }
 
-inline Bitboard operator ~ (const Bitboard & a) { return Bitboard( ~(a.getBoard()) ); }
+/* Stream operator overloading */
+inline std::ostream & operator << (std::ostream& out, const Bitboard & b) { out << b.getBoard(); return out; }
