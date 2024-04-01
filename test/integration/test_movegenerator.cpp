cfe7d9bd006f65eac00e0229c37f4f83b85c4d60
Hunter Ducharme <hgducharme@gmail.com>
2024-04-01 09:54:48 -0700

    test_movegenerator now only uses perft for testing

--- test/integration/test_movegenerator.cpp
+++ test/integration/test_movegenerator.cpp
@@ -5,92 +5,80 @@
 
 #include "gtest/gtest.h"
 
-namespace meatball {
-namespace {
-
-// The fixture for testing the class.
-class MoveGeneratorTest : public ::testing::Test
+namespace meatball
 {
- protected:
-  // You can remove any or all of the following functions if their bodies would
-  // be empty.
+   namespace
+   {
 
-  MoveGeneratorTest()
-  {
-     // You can do set-up work for each test here.
-  }
+      // The fixture for testing the class.
+      class MoveGeneratorTest : public ::testing::Test
+      {
+      protected:
+         // You can remove any or all of the following functions if their bodies would
+         // be empty.
 
-  ~MoveGeneratorTest() override
-  {
-     // You can do clean-up work that doesn't throw exceptions here.
-  }
+         MoveGeneratorTest()
+         {
+            // You can do set-up work for each test here.
+         }
 
-  // If the constructor and destructor are not enough for setting up
-  // and cleaning up each test, you can define the following methods:
+         ~MoveGeneratorTest() override
+         {
+            // You can do clean-up work that doesn't throw exceptions here.
+         }
 
-  void SetUp() override
-  {
-     // Code here will be called immediately after the constructor (right
-     // before each test).
-  }
+         // If the constructor and destructor are not enough for setting up
+         // and cleaning up each test, you can define the following methods:
 
-  void TearDown() override
-  {
-     // Code here will be called immediately after each test (right
-     // before the destructor).
-  }
+         void SetUp() override
+         {
+            // Code here will be called immediately after the constructor (right
+            // before each test).
+         }
 
-  static void SetUpTestSuite()
-  {
-   	// Code here will be called before the first test and will not be called again.
-      attack_tables::init();
-  }
+         void TearDown() override
+         {
+            // Code here will be called immediately after each test (right
+            // before the destructor).
+         }
 
-  static void TearDownTestSuite()
-  {
-		// Code here will be called after the last test.
-  }
+         static void SetUpTestSuite()
+         {
+            // Code here will be called before the first test and will not be called again.
+            attack_tables::init();
+         }
 
-  // Class members declared here can be used by all tests in the test suite.
-  // To share resources between tests, declare them as static class members,
-  // then add their definition just below the test fixture class.
-};
+         static void TearDownTestSuite()
+         {
+            // Code here will be called after the last test.
+         }
 
+         // Class members declared here can be used by all tests in the test suite.
+         // To share resources between tests, declare them as static class members,
+         // then add their definition just below the test fixture class.
+      };
 
-// TODO: Write a perft instead of integration testing our move generation. Although
-// A test like this might be helpful in debugging and stepping through our move generation code.
-// A perft is the proper way to test a chess engine's move generation capabilities.
-TEST_F(MoveGeneratorTest, generatePsuedoLegalMoves)
-{
-   Chessboard game;
-   LegalMoveGenerator moveGenerator;
-   
-   MoveVector moves = moveGenerator.generatePsuedoLegalMoves(game);
-	game.applyMove(WHITE, PAWN, e2, e4);
-	moves = moveGenerator.generatePsuedoLegalMoves(game);
-}
+      TEST_F(MoveGeneratorTest, perft_initialPositionDepth1)
+      {
+         Chessboard game;
+         uint16_t depth = 1;
 
-TEST_F(MoveGeneratorTest, perft_initialPositionDepth1)
-{
-   Chessboard game;
-   uint16_t depth = 1;
+         u64 numberOfNodes = perft(game, depth);
 
-   u64 numberOfNodes = perft(game, depth);
+         u64 EXPECTED = 20;
+         ASSERT_EQ(numberOfNodes, EXPECTED);
+      }
 
-   u64 EXPECTED = 20;
-   ASSERT_EQ(numberOfNodes, EXPECTED);
-}
+      TEST_F(MoveGeneratorTest, perft_initialPositionDepth3)
+      {
+         Chessboard game;
+         uint16_t depth = 3;
 
-TEST_F(MoveGeneratorTest, perft_initialPositionDepth3)
-{
-   Chessboard game;
-   uint16_t depth = 3;
+         u64 numberOfNodes = perft(game, depth);
 
-   u64 numberOfNodes = perft(game, depth);
+         u64 EXPECTED = 8902;
+         ASSERT_EQ(numberOfNodes, EXPECTED);
+      }
 
-   u64 EXPECTED = 8902;
-   ASSERT_EQ(numberOfNodes, EXPECTED);
-}
-
-}  // namespace
-}  // namespace meatball
+   } // namespace
+} // namespace meatball
