fdae7f79b6a6074d9f4c4f22d63c8986d90f25c8
Hunter Ducharme <hgducharme@gmail.com>
2024-04-03 14:47:40 -0700

    Added isPawnDoublePush and isEnPessant flags to Move class

--- src/move.cpp
+++ src/move.cpp
@@ -1,9 +1,17 @@
 #include "move.h"
 
-Move::Move(Color c, PieceType p, Square start, Square end) : 
+Move::Move(const Color c, const PieceType p, const Square start, const Square end) : 
     color(c), piece(p), startSquare(start), endSquare(end)
     {}
     
-Move::Move(Color c, PieceType p, Square start, Square end, bool isPawnPromotion) : 
+Move::Move(const Color c, const PieceType p, const Square start, const Square end, const bool isPawnPromotion) : 
     color(c), piece(p), startSquare(start), endSquare(end), isPawnPromotion(isPawnPromotion)
+    {}
+
+Move::Move(const Color c, const PieceType p, const Square start, const Square end, const bool isPawnPromotion, const bool isPawnDoublePush) : 
+    color(c), piece(p), startSquare(start), endSquare(end), isPawnPromotion(isPawnPromotion), isPawnDoublePush(isPawnDoublePush)
+    {}
+
+Move::Move(const Color c, const PieceType p, const Square start, const Square end, const bool isPawnPromotion, const bool isPawnDoublePush, const bool isEnPessant) :
+    color(c), piece(p), startSquare(start), endSquare(end), isPawnPromotion(isPawnPromotion), isPawnDoublePush(isPawnDoublePush), isEnPessant(isEnPessant)
     {}
--- src/move.h
+++ src/move.h
@@ -4,13 +4,17 @@
 
 struct Move
 {
-    Move(Color c, PieceType p, Square start, Square end);
-    Move(Color c, PieceType p, Square start, Square end, bool isPawnPromotion);
+    Move(const Color c, const PieceType p, const Square start, const Square end);
+    Move(const Color c, const PieceType p, const Square start, const Square end, const bool isPawnPromotion);
+    Move(const Color c, const PieceType p, const Square start, const Square end, const bool isPawnPromotion, const bool isPawnDoublePush);
+    Move(const Color c, const PieceType p, const Square start, const Square end, const bool isPawnPromotion, const bool isPawnDoublePush, const bool isEnPessant);
     Color color;
     PieceType piece;
     Square startSquare;
     Square endSquare;
     bool isPawnPromotion = false;
+    bool isPawnDoublePush = false;
+    bool isEnPessant = false;
 };
 
 inline bool operator == (const Move & lhs, const Move & rhs)
