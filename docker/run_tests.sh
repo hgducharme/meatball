aa32685b10e69dcaaaff01f28b4036d9d94a6d77
Hunter Ducharme <hgducharme@gmail.com>
2024-06-06 12:17:00 -0700

    Fixed errors in shell script

--- docker/run_tests.sh
+++ docker/run_tests.sh
@@ -6,4 +6,4 @@ CURRENT_DIR=$(dirname $0)
 PARENT_DIR=$(dirname $(dirname "$0"))
 
 docker build -t meatball/${IMAGE_NAME}:0.1 -f ${CURRENT_DIR}/Dockerfile.${IMAGE_NAME} ${PARENT_DIR}
-docker run --rm -t --name=meatball-${IMAGE_NAME} --mount type=bind,source=${PARENT_DIR},target=/usr/src/app meatball//${IMAGE_NAME}:0.1
+docker run --rm -t --name=meatball-${IMAGE_NAME} --mount type=bind,source=${PARENT_DIR},target=/usr/src/app meatball/${IMAGE_NAME}:0.1
