45bd30573e06b1ee46c9a52e3754ad0102674632
Hunter Ducharme <hgducharme@gmail.com>
2024-06-06 00:31:34 -0700

    Linter is mostly working

--- docker/entrypoints/linter_entrypoint.sh
+++ docker/entrypoints/linter_entrypoint.sh
@@ -1,6 +1,21 @@
 #!/bin/sh -l
 
 # cppcheck
+echo "**************************************************************************************************************"
+echo ""
+echo "cppcheck output"
+echo ""
+echo "**************************************************************************************************************"
 cppcheck src --enable=all --suppress=missingIncludeSystem --suppress=unusedFunction --suppress=unmatchedSuppression --check-level=exhaustive
 
-# TODO: clang-tidy
+echo "**************************************************************************************************************"
+echo ""
+echo "clang-tidy output"
+echo ""
+echo "**************************************************************************************************************"
+
+export CPP_SRC_FILES=$(find /usr/src/app/src -name "*.*" | grep -E "(\.cpp$|\.h$|$)")
+if [ -n "$CPP_SRC_FILES" ]; then clang-tidy -checks='cppcoreguidelines-*,performance-*,readibility-*,modernize-*,misc-*,clang-analyzer-*,-modernize-use-trailing-return-type' --header-filter=*.h --export-fixes=clang-tidy-fixes $CPP_SRC_FILES; echo $?; fi;
+
+
+# find . -name '*.cpp' -o -name '*.h' | xargs clang-tidy
