f54e05458cdec104c93c8ea6ee6e9021a9f1849a
Hunter Ducharme <hgducharme@gmail.com>
2024-06-06 19:56:01 -0700

    Turned version number into a variable

--- docker/run_tests.sh
+++ docker/run_tests.sh
@@ -2,9 +2,10 @@
 set -e
 
 IMAGE_NAME=tests
+VERSION=0.1
 
 CURRENT_DIR=$(dirname $0)
 PARENT_DIR=$(dirname $(dirname "$0"))
 
-docker build -t meatball/${IMAGE_NAME}:0.1 -f ${CURRENT_DIR}/Dockerfile.${IMAGE_NAME} ${PARENT_DIR}
-docker run --rm -t --name=meatball-${IMAGE_NAME} --mount type=bind,source=${PARENT_DIR},target=/usr/src/app meatball/${IMAGE_NAME}:0.1
+docker build -t meatball/${IMAGE_NAME}:${VERSION} -f ${CURRENT_DIR}/Dockerfile.${IMAGE_NAME} ${PARENT_DIR}
+docker run --rm -t --name=meatball-${IMAGE_NAME} --mount type=bind,source=${PARENT_DIR},target=/usr/src/app meatball/${IMAGE_NAME}:${VERSION}
