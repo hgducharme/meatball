c795cec740ccfacbce2493738384a6fa2a7713fa
Hunter Ducharme <hgducharme@gmail.com>
2024-06-09 19:08:42 -0700

    PUCT rough draft

--- src/move_search/mcts/mcts.cpp
+++ src/move_search/mcts/mcts.cpp
@@ -2,17 +2,27 @@
 
 Move MonteCarloTreeSearch::getBestMove(const Chessboard & gameState)
 {
-   /*
-   Node root = gameState;
-   while (timeRemaining)
+   Node root;
+   root.currentPlayer = gameState.getActivePlayer();
+   
+   // while (timeRemaining)
+   for (int i = 0; i < 2; i++)
    {
-      Node leaf = select(root);
-      if (leaf.endsGame) { continute; }
-      expand(leaf);
-      (leaf_value, policy) = simulate(leaf); // lc0's "policy" is a list of moves and a probability for each move. see here: https://lczero.org/dev/wiki/technical-explanation-of-leela-chess-zero/
-      backpropgate(leaf, result);
+      Node leaf = selectChildNode(root);
+      if (leaf.endsGame) { continue; }
+      // expand(leaf);
+      // (leaf_value, policy) = simulate(leaf); // lc0's "policy" is a list of moves and a probability for each move. see here: https://lczero.org/dev/wiki/technical-explanation-of-leela-chess-zero/
+      // backpropgate(leaf, result);
    }
 
-   return bestMove(root);
-   */
+   // return bestMove(root);
+}
+
+Node MonteCarloTreeSearch::selectChildNode(Node root)
+{
+   for (const Node & child : root.children)
+   {
+      // int sumOfAllSiblingVisitCounts = sum(root.children, unless c == child);
+      // int puct = child.currentPlayer * averageUtilityOf(child) + ( cpuct * policyOf(child) * (1 / (1 + child.visits)) * sqrt( sumOfAllSiblingVisitCounts ) );
+   }
 }
--- src/move_search/mcts/node.h
+++ src/move_search/mcts/node.h
@@ -1,3 +1,11 @@
 #pragma once
 
-struct Node {};
+#include "types.h"
+
+#include <vector>
+
+struct Node {
+   Color currentPlayer;
+   bool endsGame;
+   std::vector<Node> children;
+};
